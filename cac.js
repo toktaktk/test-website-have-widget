8c89b4188e2895ad9e8ed88cd8c02f037ce0c842
Pun <puncorp.app@gmail.com>
2024-04-17 17:40:54 +0700

    update cac.js

---
+++ cac.js
@@ -0,0 +1,40 @@
+(function() {    
+    // Apply styles to the FAB frame
+    const fabFrame = document.createElement('iframe');
+
+    fabFrame.id = "fab-frame";
+    fabFrame.style.position = 'fixed';
+    fabFrame.style.bottom = '20px';
+    fabFrame.style.right = '20px';
+    fabFrame.style.width = '60px';
+    fabFrame.style.height = '60px';
+    fabFrame.style.zIndex = '1100';
+    fabFrame.style.border = 'none';
+    fabFrame.src="fab.html"
+    
+    // Apply styles to the chat box frame
+    const chatboxFrame = document.createElement('iframe');
+    chatboxFrame.id = "chatbox-frame";
+    chatboxFrame.style.display = 'none'; // Hidden by default
+    chatboxFrame.style.position = 'fixed';
+    chatboxFrame.style.bottom = '90px';
+    chatboxFrame.style.right = '20px';
+    chatboxFrame.style.width = '350px';
+    chatboxFrame.style.height = '400px';
+    chatboxFrame.style.border = '1px solid #ccc';
+    chatboxFrame.style.zIndex = '1000';
+    chatboxFrame.src = "chatbox.html";
+
+    if(document.body != null){
+        document.body.appendChild(fabFrame);
+        document.body.appendChild(chatboxFrame);
+    }
+    
+        window.addEventListener('message', function(event) {
+            if (event.data === 'toggle-chatbox') {
+                var chatBoxFrame = document.getElementById('chatbox-frame');
+                chatBoxFrame.style.display = chatBoxFrame.style.display === 'none' ? 'block' : 'none';
+            }
+        });
+
+})();
--- embed.js
+++ embed.js
@@ -2,21 +2,26 @@
   const chatWidget = document.createElement('iframe');
   chatWidget.src = `https://test-run-winget.vercel.app/#widget-container`;
   chatWidget.style.border = 'none';
-  // chatWidget.style.width = '300px';
-  // chatWidget.style.height = '400px';
+  chatWidget.style.width = '80px'
+  chatWidget.style.height = '80px'
+  chatWidget.style.backgroundColor = 'red'
   chatWidget.style.position = 'fixed';
   chatWidget.style.bottom = '20px';
   chatWidget.style.right = '20px';
   chatWidget.style.zIndex = '9999';
 
   const container = document.createElement('div');
+  container.style.backgroundColor = 'green'
   container.appendChild(chatWidget);
 
   const toggleButton = document.createElement('button');
   // toggleButton.textContent = 'Open Chat';
-  // toggleButton.onclick = () => {
-  //   container.style.display = container.style.display === 'none' ? 'block' : 'none';
-  // };
+  chatWidget.onclick = () => {
+    container.style.display = container.style.display === 'none' ? 'block' : 'none';
+    chatWidget.style.width = '1000px'
+    chatWidget.style.height = '1000px'
+    container.style.width = '1000px'
+    container.style.height = '1000px'  };
 
   // document.body.appendChild(toggleButton);
   document.body.appendChild(container);
